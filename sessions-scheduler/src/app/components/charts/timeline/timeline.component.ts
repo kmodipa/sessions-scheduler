70950e769253f7b47d79fc7edca9127733a7e28e
khomotjo <khomotjomodipa4@gmail.com>
2021-10-17 22:26:06 +0200

    timeline plot

--- sessions-scheduler/src/app/components/charts/timeline/timeline.component.ts
+++ sessions-scheduler/src/app/components/charts/timeline/timeline.component.ts
@@ -1,15 +1,76 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnChanges, OnInit, SimpleChanges, ViewChild} from '@angular/core';
+import {
+  ChartComponent,
+  ApexAxisChartSeries,
+  ApexChart,
+  ApexPlotOptions,
+  ApexXAxis
+} from "ng-apexcharts";
+import {SessionModel} from "../../../core/models/Session.Model";
+
+export type ChartOptions = {
+  series: ApexAxisChartSeries;
+  chart: ApexChart;
+  xaxis: ApexXAxis;
+  plotOptions: ApexPlotOptions;
+};
+
+export type SessionData = {
+  x: string;
+  y: []
+}
 
 @Component({
   selector: 'app-timeline',
   templateUrl: './timeline.component.html',
   styleUrls: ['./timeline.component.scss']
 })
-export class TimelineComponent implements OnInit {
+export class TimelineComponent implements OnChanges {
 
-  constructor() { }
+  @ViewChild("chart") chart!: ChartComponent;
+  public chartOptions: Partial<ChartOptions> | any;
+  @Input() sessions: SessionModel[] | any;
+  public sessionData: SessionData[] | any;
+
+  constructor() {
+    this.sessionData = Array<SessionData>();
+  }
 
   ngOnInit(): void {
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(this.sessions.length);
+
+    for (let i = 0; i < this.sessions.length; i++) {
+      this.sessionData.push({
+        x : this.sessions[i].name,
+        y : [
+          this.sessions[i].start.getTime(),
+          this.sessions[i].end.getTime()
+        ]
+      });
+    }
+
+    this.chartOptions = {
+      series: [
+        {
+          data: this.sessionData
+        }
+      ],
+      chart: {
+        height: 250,
+        type: "rangeBar"
+      },
+      plotOptions: {
+        bar: {
+          horizontal: true
+        }
+      },
+      xaxis: {
+        type: "datetime"
+      }
+    };
   }
 
 }
